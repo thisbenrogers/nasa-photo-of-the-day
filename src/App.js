e06a189834d7faa6c71a77f11c04712201a7c350
BenR <20133902+thisbenrogers@users.noreply.github.com>
2019-07-17 15:24:06 -0500

    :art: Refactors logic into App.js, passes down via props.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+
 
 import APODPhoto from './components/APODPhoto';
 import Header from './components/Header';
@@ -6,10 +9,24 @@ import Header from './components/Header';
 import "./App.css";
 
 function App() {
+  const [apodData, setApodData] = useState({});
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=acaE6QqfjcI4Nqk2RdyPiuXYlKpuFaSPObtD6KXp")
+      .then(response => {
+        console.log("response.data:", response.data);
+        const apod = response.data;
+        setApodData(apod);
+      })
+      .catch(err => {
+        console.log("There was an error: ", err);
+      })
+  }, [])
+
   return (
     <div className="App">
       <Header />
-      <APODPhoto />
+      <APODPhoto apodData={apodData} />
     </div>
   );
 }
--- src/components/APODPhoto.js
+++ src/components/APODPhoto.js
@@ -1,30 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player'
-import axios from 'axios';
 
 
-function APODPhoto() {
-  const [apodData, setApodData] = useState({});
 
-  useEffect(() => {
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=acaE6QqfjcI4Nqk2RdyPiuXYlKpuFaSPObtD6KXp")
-      .then(response => {
-        console.log("response.data:", response.data);
-        const apod = response.data;
-        setApodData(apod);
-      })
-      .catch(err => {
-        console.log("There was an error: ", err);
-      })
-  }, [])
+function APODPhoto(props) {
 
-  // TODO edit with ternary to also display video
+
   return (
     <section className="cards-list">
       {
-        apodData.media_type === "video" ?
-          <ReactPlayer className="media" url={apodData.url} playing /> :
-          <img className="media" src={apodData.url} alt="Nasa Astonomy Photo of the Day" />
+        props.apodData.media_type === "video" ?
+          <ReactPlayer className="media" url={props.apodData.url} playing /> :
+          <img className="media" src={props.apodData.url} alt="Nasa Astonomy Photo of the Day" />
       }
     </section>
   )
