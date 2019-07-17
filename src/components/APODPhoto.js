8ecfd51e535e20a6b6b3cc07d24f0a1f6a869e7e
BenR <20133902+thisbenrogers@users.noreply.github.com>
2019-07-17 14:44:28 -0500

    :sparkles: Adds useEffect

--- src/components/APODPhoto.js
+++ src/components/APODPhoto.js
@@ -5,21 +5,27 @@ import axios from 'axios';
 function APODPhoto() {
   const [apodData, setApodData] = useState({});
 
-  // useEffect(() => {
-  //   axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-  //     .then(response => {
-  //       console.log("response.data:", response.data);
-  //       const apod = response.data;
-  //       setApodData(apod);
-  //     })
-  //     .catch(err => {
-  //       console.log("There was an error: ", err);
-  //     })
-  // }, [apodData])
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=acaE6QqfjcI4Nqk2RdyPiuXYlKpuFaSPObtD6KXp")
+      .then(response => {
+        console.log("response.data:", response.data);
+        const apod = response.data;
+        setApodData(apod);
+      })
+      .catch(err => {
+        console.log("There was an error: ", err);
+      })
+  }, [])
 
+  // TODO edit with ternary to also display video
   return (
     <section className="cards-list">
       <p>APOD Photo</p>
+      {/* {
+        apodData.media_type === "video" ?
+          render video here :
+          <img src={apodData.url} alt="Nasa Astonomy Photo of the Day" />
+      } */}
     </section>
   )
 
