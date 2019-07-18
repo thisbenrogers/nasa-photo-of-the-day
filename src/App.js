04625330ac27ef8bb06dcf19e75c2e021bbcd8d0
BenR <20133902+thisbenrogers@users.noreply.github.com>
2019-07-18 12:29:32 -0500

    :sparkles: Finishes styling, edits manifest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import { Divider, Grid, Segment } from 'semantic-ui-react'
+import { Divider, Grid, Segment, Icon } from 'semantic-ui-react'
+import styled from 'styled-components';
 
 import APODPhoto from './components/APODPhoto';
 import TextData from './components/TextData';
@@ -8,6 +9,15 @@ import TextData from './components/TextData';
 import "./App.css";
 
 function App() {
+
+  const Wrapper = styled.div`
+    height: 100vh;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  `
+
   const [apodData, setApodData] = useState({});
 
   useEffect(() => {
@@ -23,18 +33,20 @@ function App() {
   }, [])
 
   return (
-    <Segment className="App">
-      <Grid verticalAlign='middle' columns={2} relaxed="very">
-        <Grid.Column>
-          <TextData apodData={apodData} />
-        </Grid.Column>
-        <Grid.Column>
-          <APODPhoto apodData={apodData} />
-        </Grid.Column>
-      </Grid>
+    <Wrapper>
+      <Segment className="App">
+        <Grid verticalAlign='middle' columns={2} relaxed="very">
+          <Grid.Column>
+            <TextData apodData={apodData} />
+          </Grid.Column>
+          <Grid.Column>
+            <APODPhoto apodData={apodData} />
+          </Grid.Column>
+        </Grid>
 
-      <Divider vertical />
-    </Segment>
+        <Divider vertical> <Icon name="space shuttle" /></Divider>
+      </Segment>
+    </Wrapper>
   );
 }
 
--- src/components/TextData.js
+++ src/components/TextData.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Header, Icon } from 'semantic-ui-react';
 
-function TextData() {
+
+function TextData(props) {
+
+  // console.log("apodData: ", props.apodData);
+
   return (
     <>
-      <Header as="h1">Nasa's APOD <Icon name="space shuttle" /></Header>
-      <Header as="h2">(Astronomy Photo of the day)</Header>
+      <Header as="h1">Nasa's APOD</Header>
+      <p>(Astronomy Photo of the day)</p>
+      <Header as="h2">"{props.apodData.title}"</Header>
+      <p>Date: {props.apodData.date}</p>
+      <p>{props.apodData.explanation}</p>
     </>
   )
 }
